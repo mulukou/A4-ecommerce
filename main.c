50ede0f33b072bd82bfe1774b697987ac9fac056
mulukou <mulukou@gmail.com>
2020-06-22 12:31:11 -0300

    Pequenas mudanças, novo bug: Categorias aparece 2 vezes antes de pedir por comando

--- main.c
+++ main.c
@@ -4,31 +4,43 @@
 
 int main()
 {
+    printf("\n\n                                                Olá, Seja Bem Vindo(a) à nossa loja!\n\n");
     int whileloop = 0;
     while(whileloop <= 0)
     {
         char operation;
 
         printf("=========================================================================================================================================================\n\n");
-        printf("\n\n                                                Olá, Seja Bem Vindo(a) à nossa loja!\n\n");
-        printf("\n\n              Use esses comandos para se localizar: (C)ategorias (A)dicionar ao Carrinho (F)inalizar a Compra (S)air do Aplicativo");
-        printf("\n=========================================================================================================================================================\n\n");
+ 
+        printf("\n\n              Use esses comandos para se localizar: (C)ategorias (A)dicionar ao Carrinho (F)inalizar a Compra (S)air do Aplicativo\n\n");
+        printf("\n\n=========================================================================================================================================================\n\n");
         printf("Insira seu comando: ");
-        scanf("%c", &operation);
+        scanf(" %c", &operation);
 
         if(operation == 'C')
         {
-            printf("=========================================================================================================================================================\n\n");
-            printf("\n\n                                                                Categorias\n\n");
-            printf("\n\n              (C)adernos (Ca)netas (L)ápis (Li)vros (M)ochilas (E)stojos (T)intas (I)nstrumentos Musicais");
-            printf("\n=========================================================================================================================================================\n\n");
+            int funcionando = 0;
+            while (funcionando <= 0)
+            {
+                printf("\n\n=========================================================================================================================================================\n\n");
+                printf("\n\n                                                                Categorias\n\n");
+                printf("\n\n              (C)adernos (Ma)terias Escolares (Li)vros (M)ochilas (E)stojos (T)intas (I)nstrumentos Musicais");
+                printf("\n=========================================================================================================================================================\n\n");
+                printf("Insira seu comando: ");
+                scanf("%c", &operation);
+
+                if(operation == 'C')
+                {
+                    printf("\n\n Você está na sessão de cadernos");
+                }
+            }
         }
         // Mover para dentro da página do produto
-        else if (operation == 'A')
+        else if(operation == 'A')
         {
-
+            printf("\n\nTestando o botão A");
         }
-        else if (operation == 'F')
+        else if(operation == 'F')
         {
 
         }
